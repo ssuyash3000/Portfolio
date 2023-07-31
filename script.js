f9c13c7b9ab92c12677be8c02cb9ca14a066ec85
ssuyash3000 <ssuyash2001@gmail.com>
2023-07-31 23:55:45 +0530

    Adds new resume

--- script.js
+++ script.js
@@ -9,34 +9,34 @@ setInterval(() => {
   } else heading.innerHTML = "Portfolio in making...";
   counter++;
   counter %= 3;
-}, 350);
+}, 550);
 let resume = document.getElementById("resume");
 resume.addEventListener("click", () => {
-  window.open("./assets/Suyash's Resume.pdf", "_blank");
+  window.open("./assets/Suyash_2023_Grad_Final.pdf", "_blank");
 });
 setInterval(() => {
+  resume.style.border = "5px solid blue";
   if (counter1 === 0) {
     resume.style.borderTop = "5px solid blue";
-    resume.style.borderLeft = "5px solid white";
+    resume.style.borderLeft = "5px solid red";
     resume.style.borderRight = "5px solid white";
     resume.style.borderBottom = "5px solid white";
   } else if (counter1 === 1) {
     resume.style.borderRight = "5px solid blue";
     resume.style.borderLeft = "5px solid white";
-    resume.style.borderTop = "5px solid white";
+    resume.style.borderTop = "5px solid red";
     resume.style.borderBottom = "5px solid white";
   } else if (counter1 === 2) {
     resume.style.borderBottom = "5px solid blue";
     resume.style.borderTop = "5px solid white";
-    resume.style.borderRight = "5px solid white";
+    resume.style.borderRight = "5px solid red";
     resume.style.borderLeft = "5px solid white";
   } else {
     resume.style.borderLeft = "5px solid blue";
     resume.style.borderTop = "5px solid white";
     resume.style.borderRight = "5px solid white";
-    resume.style.borderBottom = "5px solid white";
+    resume.style.borderBottom = "5px solid red";
   }
-
   counter1++;
   counter1 %= 4;
-}, 550);
+}, 750);
